d0facc17d988bb2d95ee95e8dec0484912318acb
Knob <59198478+nbr41to@users.noreply.github.com>
2020-07-03 15:01:28 +0900

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { Link ,graphql } from "gatsby"
+// GraphQLインポートしてないよ
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Markdown Pages</h1>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <article key={node.frontmatter.slug}>
+        <h2>
+          <Link to={`/${node.frontmatter.slug}`}>
+            {node.frontmatter.title}
+          </Link>
+        </h2>
+        <time dateTime={node.frontmatter.date}>{node.frontmatter.date}</time>
+        <p>{node.excerpt}</p>
+      </article>
+    ))}
+    <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+  </Layout>
+)
+
+export const query = graphql`
+  query {
+    allMarkdownRemark (sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            date(formatString: "YYYY年MM月DD日")
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+export default IndexPage
